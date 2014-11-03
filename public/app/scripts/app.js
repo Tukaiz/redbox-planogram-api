18ffb45b17036665f2ed3534af50234f5b1aa28b
Eric Agnew <wenga.eric@gmail.com>
2014-11-03 12:24:58 -0600

    Init Angular Commit;

---
+++ public/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name planogramAppApp
+ * @description
+ * # planogramAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('planogramAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ public/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name planogramAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the planogramAppApp
+ */
+angular.module('planogramAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
